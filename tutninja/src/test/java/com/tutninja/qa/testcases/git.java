7a79d3e84626daf25bec888f756fea060934038b
swami koragajja <swami koragajja@LAPTOP-9NE0LV8V>
2024-01-21 20:24:39 +0530

    updated

---
+++ tutninja/src/test/java/com/tutninja/qa/testcases/git.java
@@ -0,0 +1,11 @@
+package com.tutninja.qa.testcases;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello pavan");
+
+	}
+
+}
